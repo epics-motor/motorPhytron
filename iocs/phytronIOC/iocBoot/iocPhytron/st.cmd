cb58e209b3cb8115c43f58c002dbb86e3d009438
kpetersn <kmpeters@anl.gov>
2019-04-10 13:50:52 -0500

    Improved the example IOC

--- iocs/phytronIOC/iocBoot/iocPhytron/st.cmd
+++ iocs/phytronIOC/iocBoot/iocPhytron/st.cmd
@@ -1,8 +1,5 @@
 #!../../bin/linux-x86_64/phytron
 
-## You may have to change phytron to something else
-## everywhere it appears in this file
-
 < envPaths
 
 cd "${TOP}"
@@ -11,11 +8,16 @@ cd "${TOP}"
 dbLoadDatabase "dbd/phytron.dbd"
 phytron_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadRecords("db/xxx.db","user=kpetersn")
-
 cd "${TOP}/iocBoot/${IOC}"
+
+## motorUtil (allstop & alldone)
+dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=phytron:")
+
+## 
+
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=kpetersn"
+## motorUtil (allstop & alldone)
+motorUtilInit("phytron:")
+
+# Boot complete
